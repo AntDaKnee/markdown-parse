7874fcdc66d00a4a53a845dcad6bbd1f9e24c8d8
AntDaKnee <akgonzalez@ucsd.edu>
2022-01-20 19:14:02 -0800

    Fixed 1st error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,6 +19,9 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             System.out.println("Open paren value " + openParen);
             System.out.println("Close paren value " + closeParen);
+            if(openParen == -1 || closeParen == -1){
+                return toReturn;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
